922468f9435783e54c80043a75df26a8fc16f90b
comanchechao <61477751+comanchechao@users.noreply.github.com>
2021-01-11 04:07:02 +0330

    Shopping cart design almost done

--- client/components/ShoppingCartDrawer.vue
+++ client/components/ShoppingCartDrawer.vue
@@ -25,23 +25,33 @@
       hide-overlay
       app
       scrollable
-      src="/tyler-nix-6mze64HRU2Q-unsplash.jpg"
+      src="/ateke-iranmanesh-7APM4YyW_JU-unsplash.jpg"
       width="20%"
     >
-      <div class="list">
-        <div class="items">
-          <h3 class="itemName">Custom Dragon Lighter</h3>
-          <p class="price">$35000</p>
-          <div class="itemCount">
-            <v-btn small fab class="minus" color="transparent">
-              <v-icon>mdi-minus-circle-outline</v-icon></v-btn
-            >
-            <p>X2</p>
-            <v-btn small fab class="plus" color="transparent"
-              ><v-icon>mdi-plus-circle-outline</v-icon></v-btn
-            >
-          </div>
-        </div>
+      <v-container grid-list-xs class="list">
+        <v-row justify-center align-xs>
+          <v-col align-self="center" class="items my-4">
+            <h3 class="itemName my-3">Custom Dragon Lighter</h3>
+            <p class="price">$35000</p>
+
+            <div class="d-flex pa-3 justify-center">
+              <v-btn small fab class="minus" color="transparent">
+                <v-icon>mdi-minus-circle-outline</v-icon></v-btn
+              >
+              <p class="pa-3">X2</p>
+              <v-btn small fab class="plus" color="transparent"
+                ><v-icon>mdi-plus-circle-outline</v-icon></v-btn
+              >
+            </div>
+          </v-col>
+          <!-- <v-spacer></v-spacer>
+          <v-col justify-center>
+          </v-col> -->
+        </v-row>
+      </v-container>
+      <v-spacer></v-spacer>
+      <div class="checkout d-flex justify-center align-end">
+        <v-btn x-large color="blue">Checkout</v-btn>
       </div>
     </v-navigation-drawer>
   </div>
@@ -57,4 +67,31 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;600&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap');
+
+.list {
+  width: 300px;
+  height: 800px;
+}
+
+.items {
+  background-color: rgb(5, 0, 32);
+  box-shadow: black 1px;
+  border-radius: 10px;
+}
+
+.itemName {
+  font-family: 'Poppins', sans-serif;
+  font-size: 1.5rem;
+  margin-bottom: 10px;
+  line-height: 30px;
+}
+
+/* .checkout {
+  display: flex;
+  justify-self: center;
+} */
+</style>
