91dc07475c4bc7ffe1bbe12bd62e9278793b8230
Wronghand-hash <62405360+Wronghand-hash@users.noreply.github.com>
2021-04-27 15:28:36 +0430

    update index.js

--- client/store/index.js
+++ client/store/index.js
@@ -1,160 +1,154 @@
-/* eslint-disable no-console */
-import Vuex from 'vuex'
 import axios from 'axios'
 // import firebase from 'firebase/app'
 // import 'firebase/auth'
 
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      user: null,
-      account: null,
-      products: [],
-      cart: [],
-    },
+export const state = () => ({
+  user: null,
+  account: null,
+  products: [],
+  cart: [],
+})
 
-    getters: {
-      getUser: (state) => state.user,
+export const mutations = {
+  // setUser: (state) => {
+  //   state.user = this.$fire.auth.currentUser
+  // },
+  setUpProducts(state, productsPayload) {
+    // sets the state's  products property to the products array recieved as payload
+    state.products = productsPayload
+  },
+  // set products in store states as array
+  setProducts(state, products) {
+    state.products = products
+  },
+  AddToCart(state, { product, quantity }) {
+    const productInCart = state.cart.find(
+      (item) => item.product._id === product._id
+    )
 
-      // open some place for this big G
-      cartItemCount: (state) => {
-        return state.cart.length
-      },
+    console.log(productInCart)
 
-      // use this in the shopping cart drawer
-      cartTotalPrice: (state) => {
-        let total = 0
+    if (productInCart) {
+      productInCart.quantity += quantity
+      return
+    }
 
-        state.cart.forEach((item) => {
-          total = item.product.price * item.quantity
-        })
-
-        return total
-      },
-
-      cartSize: (state) => {
-        return state.cart.length
-      },
-      cartTotalAmount: (state) => {
-        return state.cart.reduce((total, product) => {
-          return total + product.price * product.quantity
-        }, 0)
-      },
-      toast: (state) => {
-        return state.toast
-      },
-    },
-
-    mutations: {
-      // setUser: (state) => {
-      //   state.user = this.$fire.auth.currentUser
-      // },
-      setUpProducts: (state, productsPayload) => {
-        // sets the state's  products property to the products array recieved as payload
-        state.products = productsPayload
-      },
-      // set products in store states as array
-      setProducts: (state, products) => {
-        state.products = products
-      },
-      AddToCart: (state, { product, quantity }) => {
-        const productInCart = state.cart.find(
-          (item) => item.product._id === product._id
-        )
-
-        console.log(productInCart)
-
-        if (productInCart) {
-          productInCart.quantity += quantity
-          return
-        }
-
-        state.cart.push({
-          product,
-          quantity,
-        })
-      },
-      removeProduct: (state, product) => {
-        state.cart = state.cart.filter((item) => {
-          return item.product._id !== product._id
-        })
-      },
-    },
-    actions: {
-      //  fetching the products from server side passing to set products mutaions
-      getProducts: ({ commit }) => {
-        this.$fire.firestore
-          .collection('Products')
-          .get()
-          .then((snapshot) => {
-            commit(
-              'setProducts',
-              snapshot.docs.map((doc) => doc.data())
-            )
-          })
-      },
-      addProductToCart: ({ commit }, { product, quantity }) => {
-        commit('AddToCart', { product, quantity })
-
-        axios.post('http://localhost:4000/api/orders', {
-          product: product._id,
-          quantity,
-        })
-      },
-
-      removeCartProduct: ({ commit }, product) => {
-        commit('removeProduct', product)
-      },
-
-      signUp({ commit }, { email, password }) {
-        return this.$fire.auth.createUserWithEmailAndPassword(email, password)
-        // .then(() => {
-        //   const user = this.$fire.auth.currentUser
-        //   const actionCodeSettings = {
-        //     url: `${process.env.VUE_APP_HOST_NAME}/sign-in/?email=${user.email}`,
-        //   }
-        //   user.sendEmailVerification(actionCodeSettings)
-        // })
-      },
-
-      signInWithEmail({ commit }, { email, password }) {
-        return this.$fire.auth.signInWithEmailAndPassword(email, password)
-      },
-
-      signOut() {
-        return this.$fire.auth.signOut()
-      },
-      // setUser: (context) => {
-      //   context.commit('setUser')
-      // },
-      //   fetchProducts: ({ commit }) => {
-      //     // simulating a fake ajax request to fetch products from database
-      //     myApi.getProducts().then((products) => {
-      //       // passing the products recieved from response as a payload to the mutation
-      //       commit('setUpProducts', products)
-      //       commit('showToast', 'products loaded')
-      //     })
-      //   },
-      //   addToCart: ({ commit }, productId) => {
-      //     myApi.products('add', productId).then((productId) => {
-      //       commit('addToCart', productId)
-      //       commit('showToast', 'added to cart')
-      //     })
-      //   },
-      //   removeFromCart: ({ commit }, productId) => {
-      //     myApi.products('remove', productId).then((productId) => {
-      //       commit('removeFromCart', productId)
-      //       commit('showToast', 'removed from cart')
-      //     })
-      //   },
-      //   deleteFromCart: ({ commit }, productId) => {
-      //     myApi.products('delete', productId).then((productId) => {
-      //       commit('deleteFromCart', productId)
-      //       commit('showToast', 'deleted from cart')
-      //     })
-      //   },
-    },
-  })
+    state.cart.push({
+      product,
+      quantity,
+    })
+  },
+  removeProduct(state, product) {
+    state.cart = state.cart.filter((item) => {
+      return item.product._id !== product._id
+    })
+  },
 }
+export const actions = {
+  //  fetching the products from server side passing to set products mutaions
+  getProducts({ commit }) {
+    this.$fire.firestore
+      .collection('Products')
+      .get()
+      .then((snapshot) => {
+        commit(
+          'setProducts',
+          snapshot.docs.map((doc) => doc.data())
+        )
+      })
+  },
+  addProductToCart({ commit }, { product, quantity }) {
+    commit('AddToCart', { product, quantity })
 
-export default createStore
+    axios.post('http://localhost:4000/api/orders', {
+      product: product._id,
+      quantity,
+    })
+  },
+
+  removeCartProduct({ commit }, product) {
+    commit('removeProduct', product)
+  },
+
+  signUp({ commit }, { email, password }) {
+    return this.$fire.auth.createUserWithEmailAndPassword(email, password)
+    // .then(() => {
+    //   const user = this.$fire.auth.currentUser
+    //   const actionCodeSettings = {
+    //     url: `${process.env.VUE_APP_HOST_NAME}/sign-in/?email=${user.email}`,
+    //   }
+    //   user.sendEmailVerification(actionCodeSettings)
+    // })
+  },
+
+  signInWithEmail({ commit }, { email, password }) {
+    return this.$fire.auth.signInWithEmailAndPassword(email, password)
+  },
+
+  signOut() {
+    return this.$fire.auth.signOut()
+  },
+
+  // setUser: (context) => {
+  //   context.commit('setUser')
+  // },
+  //   fetchProducts: ({ commit }) => {
+  //     // simulating a fake ajax request to fetch products from database
+  //     myApi.getProducts().then((products) => {
+  //       // passing the products recieved from response as a payload to the mutation
+  //       commit('setUpProducts', products)
+  //       commit('showToast', 'products loaded')
+  //     })
+  //   },
+  //   addToCart: ({ commit }, productId) => {
+  //     myApi.products('add', productId).then((productId) => {
+  //       commit('addToCart', productId)
+  //       commit('showToast', 'added to cart')
+  //     })
+  //   },
+  //   removeFromCart: ({ commit }, productId) => {
+  //     myApi.products('remove', productId).then((productId) => {
+  //       commit('removeFromCart', productId)
+  //       commit('showToast', 'removed from cart')
+  //     })
+  //   },
+  //   deleteFromCart: ({ commit }, productId) => {
+  //     myApi.products('delete', productId).then((productId) => {
+  //       commit('deleteFromCart', productId)
+  //       commit('showToast', 'deleted from cart')
+  //     })
+  //   },
+}
+export const getters = {
+  getUser: (state) => {
+    return state.user
+  },
+
+  // open some place for this big G
+  cartItemCount: (state) => {
+    return state.cart.length
+  },
+
+  // use this in the shopping cart drawer
+  cartTotalPrice(state) {
+    let total = 0
+
+    state.cart.forEach((item) => {
+      total = item.product.price * item.quantity
+    })
+
+    return total
+  },
+
+  cartSize(state) {
+    return state.cart.length
+  },
+  cartTotalAmount: (state) => {
+    return state.cart.reduce((total, product) => {
+      return total + product.price * product.quantity
+    }, 0)
+  },
+  toast: (state) => {
+    return state.toast
+  },
+}
