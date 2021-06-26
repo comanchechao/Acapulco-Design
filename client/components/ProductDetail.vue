87a8e5ba0dfa99be98304c21c86a29e21eaefb95
comanchechao <61477751+comanchechao@users.noreply.github.com>
2021-06-26 19:20:43 +0330

    malmoolin

--- client/components/ProductDetail.vue
+++ client/components/ProductDetail.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="w-11/12 h-dialog"></div>
+  <div class="w-11/12 h-dialog flex space-x-5">
+    <div class="bg-yellow-100 w-2/4"></div>
+    <div class="bg-Lime-200 w-2/4"></div>
+  </div>
 </template>
 
 <script>
