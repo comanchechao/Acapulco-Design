8d4394dfe79f579e0457dc3190bd94e1312c9da6
comanchechao <61477751+comanchechao@users.noreply.github.com>
2021-08-08 12:14:03 +0330

    Update ProfilePageDialog.vue

--- client/components/ProfilePageDialog.vue
+++ client/components/ProfilePageDialog.vue
@@ -29,7 +29,7 @@
             w-full
             h-full
             flex flex-col
-            justify-start
+            justify-center
             lg:justify-around
             align-center
           "
@@ -292,7 +292,7 @@ span {
 }
 
 .ordersDiv {
-  background: rgba(255, 255, 255, 0.651);
+  background: rgba(255, 255, 255, 0.2);
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
   backdrop-filter: blur(15px);
   -webkit-backdrop-filter: blur(15px);
