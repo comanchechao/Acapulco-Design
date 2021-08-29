cdbd713b396d70982c9fd78ec4c1199d53924d08
comanchechao <61477751+comanchechao@users.noreply.github.com>
2021-08-30 01:22:43 +0330

    Update index.vue

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app>
     <div id="main" class="w-screen">
-      <!-- <head>
+      <head>
         <link
           rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
           integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk"
           crossorigin="anonymous"
         />
-      </head> -->
+      </head>
       <Navbar
         id="navbar"
         v-gsap.to="{
